4ed8699ecd16c7bd6312c4ce7c10d4167fe72997
Christopher Vu <crystoffar@gmail.com>
2024-02-19 13:38:39 -0800

    Updated Contact to save into tickets table in database

--- website/app/forms.py
+++ website/app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     message = StringField('Message', validators=[DataRequired()])
     submit = SubmitField('Send')
--- website/app/routes.py
+++ website/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from app.forms import ContactForm
+from app.models import Ticket
 
 @app.route('/')
 @app.route('/index')
@@ -11,6 +12,9 @@ def index():
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
+        ticket = Ticket(name=form.name.data, email=form.email.data, message=form.message.data)
+        db.session.add(ticket)
+        db.session.commit()
         flash('Thank you, {}! Your message has been sent to The Remote Grind Team.'.format(form.name.data))
         return redirect(url_for('index'))
     return render_template('contact.html', title='Contact', form=form)
