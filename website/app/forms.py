97445cd65410068e4e75b1609e5682132c74cf97
Christopher Vu <crystoffar@gmail.com>
2024-02-26 14:17:07 -0800

    Modified Contact Page to be a two column layout, created new macro for contact page form.

--- website/app/forms.py
+++ website/app/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    name = StringField('Your Name', validators=[DataRequired()])
+    email = StringField('Email address', validators=[DataRequired(), Email()])
     message = TextAreaField('Message', validators=[DataRequired(), Length(min=1,max=512)])
     submit = SubmitField('Send')
 
