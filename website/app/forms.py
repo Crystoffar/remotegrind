989ef3e265a1dca1a18c76707cedc28f92881704
Christopher Vu <crystoffar@gmail.com>
2024-03-08 11:44:19 -0800

    formatted .py files with Black

--- website/app/forms.py
+++ website/app/forms.py
@@ -2,12 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
+
 class ContactForm(FlaskForm):
-    name = StringField('Your Name', validators=[DataRequired()])
-    email = StringField('Email address', validators=[DataRequired(), Email()])
-    message = TextAreaField('Message', validators=[DataRequired(), Length(min=1,max=512)])
-    submit = SubmitField('Send')
+    name = StringField("Your Name", validators=[DataRequired()])
+    email = StringField("Email address", validators=[DataRequired(), Email()])
+    message = TextAreaField(
+        "Message", validators=[DataRequired(), Length(min=1, max=512)]
+    )
+    submit = SubmitField("Send")
+
 
 class WaitlistForm(FlaskForm):
-    email = StringField('name@email.com', validators=[DataRequired(), Email()])
-    submit = SubmitField('Join Waitlist')
+    email = StringField("name@email.com", validators=[DataRequired(), Email()])
+    submit = SubmitField("Join Waitlist")
--- website/app/routes.py
+++ website/app/routes.py
@@ -1,32 +1,42 @@
 from flask import render_template, flash, redirect, url_for, request, jsonify
 from app import app, db
 from app.forms import ContactForm, WaitlistForm
-#from app.models import 
 from config import Config
 import requests
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
-    
     form = WaitlistForm()
     if form.validate_on_submit():
         email = form.email.data
 
         response = requests.post(
-            'https://api.getwaitlist.com/api/v1/signup',
-            json={'email': email, 'waitlist_id': Config.WAITLIST_ID, 'referral_link': request.url}
+            "https://api.getwaitlist.com/api/v1/signup",
+            json={
+                "email": email,
+                "waitlist_id": Config.WAITLIST_ID,
+                "referral_link": request.url,
+            },
         )
 
         if response.status_code == 200:
-            return jsonify({'message': 'You have been added to the waitlist!'}), 200
+            return jsonify({"message": "You have been added to the waitlist!"}), 200
         else:
-            return jsonify({'error': 'Failed to add to waitlist'}), 500
-    
-    return render_template('index.html', title='Home', form=form, active_page='home')
+            return jsonify({"error": "Failed to add to waitlist"}), 500
 
-@app.route('/contact', methods=['GET', 'POST'])
+    return render_template("index.html", title="Home", form=form, active_page="home")
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     form = ContactForm()
     formspark_id = Config.FORMSPARK_ID
-    return render_template('contact.html', title='Contact', form=form, active_page='contact', formspark_id=formspark_id)
+    return render_template(
+        "contact.html",
+        title="Contact",
+        form=form,
+        active_page="contact",
+        formspark_id=formspark_id,
+    )
