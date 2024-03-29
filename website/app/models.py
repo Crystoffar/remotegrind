d9463b137769a3266a4ff7d78d8ef924951efc8a
Christopher Vu <crystoffar@gmail.com>
2024-03-29 13:08:11 -0700

    created temporary database for Places

--- website/app/models.py
+++ website/app/models.py
@@ -2,3 +2,14 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+
+#Temporary Database for places
+class Place(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    address: so.Mapped[str] = so.mapped_column(sa.String(64))
+    latitude: so.Mapped[float] = so.mapped_column(sa.Float())
+    longitude:so.Mapped[float] = so.mapped_column(sa.Float())
+
+    def __repr__(self):
+        return '<Name {}>'.format(self.name)
